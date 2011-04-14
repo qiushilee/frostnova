cffa183ac07dbdf14f4e82f8a1e1369d5683f326
qiushilee <showbei@gmail.com>
2011-04-14 15:37:40 +0800

    consummate nova.dom.hasClass function code.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -9,10 +9,19 @@ nova.dom = {};
 
 /**
  * 检测元素是否拥有给定的 className
- * @param string
- * @param string
+ * @param {string} 元素
+ * @param {string} class 名称
  */
 nova.dom.hasClass = function(elt, cls) {
+    var eltCls = elt.className.split(" "),
+	i,
+	len;
+
+    for (i = 0, len = eltCls.length; i < len; i++) {
+	if (eltCls[i] === cls) {
+	    return true;
+	}
+    }
 };
 
 
@@ -22,9 +31,9 @@ nova.dom.hasClass = function(elt, cls) {
  * @param string
  */
 nova.dom.addClass = function(elt, cls) {
-	if (!nova.dom.hasClass(elt, cls)) {
-		elt.className === "" ? elt.className = cls : elt.className += " " +cls;
-	}
+    if (!nova.dom.hasClass(elt, cls)) {
+	elt.className === "" ? elt.className = cls : elt.className += " " +cls;
+    }
 };
 
 
@@ -34,7 +43,7 @@ nova.dom.addClass = function(elt, cls) {
  * @param string} class 名称
  */
 nova.dom.removeClass = function(elt, cls) {
-	elt.className = elt.className.replace(cls, "");
+    elt.className = elt.className.replace(cls, "");
 };
 
 
@@ -44,13 +53,13 @@ nova.dom.removeClass = function(elt, cls) {
  * @param string
  */
 nova.dom.getStyle = function(elt, styleProp, stylePropIE) {
-	if (elt.currentStyle) {
-		// IE
-		return elt.currentStyle[stylePropIE];
-	} else if (nova.WIN.getComputedStyle) {
-		// gecko
-		return document.defaultView.getComputedStyle(elt,null).getPropertyValue(styleProp);
-	}
+    if (elt.currentStyle) {
+	// IE
+	return elt.currentStyle[stylePropIE];
+    } else if (nova.WIN.getComputedStyle) {
+	// gecko
+	return document.defaultView.getComputedStyle(elt,null).getPropertyValue(styleProp);
+    }
 };
 
 
@@ -59,13 +68,13 @@ nova.dom.getStyle = function(elt, styleProp, stylePropIE) {
  * @param {string} 元素
  */
 nova.dom.getWidth = function(elt) {
-	var left,
-	    right;
+    var left,
+	right;
 
-	left = parseInt(nova.dom.getStyle(elt, "margin-left", "marginLeft"), 10);
-	right = parseInt(nova.dom.getStyle(elt, "margin-right", "marginRight"), 10);
+    left = parseInt(nova.dom.getStyle(elt, "margin-left", "marginLeft"), 10);
+    right = parseInt(nova.dom.getStyle(elt, "margin-right", "marginRight"), 10);
 
-	return elt.offsetWidth + left + right;
+    return elt.offsetWidth + left + right;
 };
 
 
