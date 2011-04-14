64385cc0ac47bf0f198fa8316754ce7ea2b25c90
qiushilee <showbei@gmail.com>
2011-04-14 15:39:18 +0800

    update version number.

--- src/frostnova/core.js
+++ src/frostnova/core.js
@@ -1,5 +1,5 @@
 /**
- * @preserve FrostNova Javascript Library v0.4.3
+ * @preserve FrostNova Javascript Library v0.5.4
  * http://qslee.com/
  *
  * Copyright 2011, Qiu-Shi Lee
@@ -32,7 +32,7 @@ nova.WIN = window;
  * The version number
  * @type string
  */
-nova.version = "0.4.3";
+nova.version = "0.5.4";
 
 
 /**
