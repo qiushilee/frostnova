0fed92ade759e24aba3280ed8d4efb7060863df1
qiushilee <showbei@gmail.com>
2011-04-14 14:59:34 +0800

    add > selector.

--- src/dom/selector.js
+++ src/dom/selector.js
@@ -4,7 +4,20 @@
 /*requires dom.js*/
 
 (function() {
-    var find = {
+    var rules,
+	find,
+	findMap,
+	tokenizer,
+	get;
+
+    rules = {
+	'id': /#((?:[\w\u00c0-\uFFFF\-]|\\.)+)/,
+	'class': /\.((?:[\w\u00c0-\uFFFF\-]|\\.)+)/,
+	'tag': /^((?:[\w\u00c0-\uFFFF\*\-]|\\.)+)/,
+	'>': /(?:(>))+/
+    };
+
+    find = {
 	byID: function(root, id) {
 	  if (root === null) {
 	      return [];
@@ -29,31 +42,60 @@
 	  }
 
 	  return results;
-      },
+        },
 
 	byClassName: function(root, className) {
-	 if (root === null) {
-	     return [];
-	 }
+	  if (root === null) {
+	      return [];
+	  }
 
-	 var i,
-	     len,
-	     results = [],
-	     nodes = root.getElementsByTagName("*");
+	  var i,
+	      len,
+	      results = [],
+	      nodes = root.getElementsByTagName("*");
 
-	 for (i = 0, len = nodes.length; i < len; i++) {
-	     var _item = nodes[i];
+	  for (i = 0, len = nodes.length; i < len; i++) {
+	      var _item = nodes[i];
+ 
+ 	      if (_item.className.match("\\b" + className + "\\b")) {
+ 	 	  results.push(_item);
+	      }
+	  }
 
-	     if (_item.className.match("\\b" + className + "\\b")) {
-		 results.push(_item);
-	     }
-	 }
+	  return results;
+       },
 
-	 return results;
-     }
+	//返回一个父级元素下的指定子级元素
+	//现仅支持一个 '>'
+       '>': function(root, part) {
+	   if (root === null) {
+	       return [];
+	   }
+
+	   var results = [];
+	   //分割选择器
+	   var selector = part.split('>');
+	   //获取父级元素
+	   var partRoot = get(selector[0], root)[0];
+
+	   //通过循环，获取右侧子级元素
+	   for (var i = 1, len = selector.length; i < len; i++) {
+	       if (rules['id'].test(results[i])) {
+		   results = findMap['id'](partRoot, selector[i]);
+
+	       } else if (rules['class'].test(selector[i])) {
+		   results = findMap['class'](partRoot, selector[i]);
+
+	       } else if (rules['tag'].test(selector[i])){
+		   results = findMap['tag'](partRoot, selector[i]);
+	       }
+	   }
+
+	   return results;
+       }
     };
 
-    var findMap = {
+    findMap = {
 	"id": function(root, selector) {
 	    selector = selector.split("#")[1];
 	    return find.byID(root, selector);
@@ -66,10 +108,14 @@
 	"class": function(root, selector) {
 	    selector = selector.split("\.")[1];
 	    return find.byClassName(root, selector);
+	},
+
+	">": function(root, selector) {
+	    return find['>'](root, selector);
 	}
     };
 
-    var tokenizer = function(selector) {
+    tokenizer = function(selector) {
 	this.selector = selector;
 	this.tokens = [];
 	this.tokenize();
@@ -78,11 +124,13 @@
     tokenizer.prototype.tokenize = function() {
 	var finder;
 
-	if (this.selector[0] === "#") {
+	if (rules['>'].test(this.selector)){
+	    finder = ">";
+	} else if (rules['id'].test(this.selector)) {
 	    finder = "id";
-	} else if (this.selector[0] === ".") {
+	} else if (rules['class'].test(this.selector)) {
 	    finder = "class";
-	} else {
+	} else if (rules['tag'].test(this.selector)){
 	    finder = "tag";
 	}
 
@@ -91,7 +139,7 @@
 	return this.tokens;
     };
 
-    var get = function(selector, root) {
+    get = function(selector, root) {
 	var tokenize = new tokenizer(selector).tokens;
 
 	return findMap[tokenize](root, selector);
--- test/selector-test.js
+++
@@ -1,3 +0,0 @@
-console.log(nova.dom.selector("#header"));
-console.log(nova.dom.selector("div"));
-console.log(nova.dom.selector(".title"));
